cc190f5f0dc45b6cd0183bf0995d50f792be7d50
Afnan Mirza <afnanmirza1999@gmail.com>
2018-08-17 22:18:41 -0400

    Initial Commit

---
+++ 20180817-AfnanMirza-NYCSchools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20180817-AfnanMirza-NYCSchools
+//
+//  Created by Afnan Mirza on 8/17/18.
+//  Copyright © 2018 Afnan Mirza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
